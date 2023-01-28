1144e23d61372f37a7c545ce55f9defd90c05744
Sam Chen <sam8211112222@gmail.com>
2023-01-28 22:33:37 +0800

    add redux toolkit query

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,8 @@
+import {useFetchAlbumsQuery} from "../store";
+
 function AlbumsList({ user }) {
+  const{data,loading,error}=useFetchAlbumsQuery(user)
+  console.log(data,loading,error)
   return <div>Albums for {user.name}</div>;
 }
 
