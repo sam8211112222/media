1e955f74d18aaadfc1d5afee7a3cd5efcf062850
Sam Chen <sam8211112222@gmail.com>
2023-01-28 23:15:26 +0800

    add mutation add tags to determine cache data need to be refetch

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,9 +1,42 @@
-import {useFetchAlbumsQuery} from "../store";
+import {useAddAlbumsMutation, useFetchAlbumsQuery} from "../store";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+import Skeleton from "./Skeleton";
 
 function AlbumsList({ user }) {
-  const{data,loading,error}=useFetchAlbumsQuery(user)
-  console.log(data,loading,error)
-  return <div>Albums for {user.name}</div>;
+  const{data,isLoading,error}=useFetchAlbumsQuery(user)
+  const [addAlbum, results] = useAddAlbumsMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+
+      return (
+          <ExpandablePanel key={album.id} header={header}>
+            List of photos in the album
+          </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+      <div>
+        <div>
+          Albums for {user.name}
+          <Button onClick={handleAddAlbum}>+ Add Album</Button>
+        </div>
+        <div>{content}</div>
+      </div>
+  );
 }
 
 export default AlbumsList;
