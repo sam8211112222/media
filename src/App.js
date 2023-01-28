1193f3c8727166aeda8a4ae0775c5cd3cb627277
Sam Chen <sam8211112222@gmail.com>
2023-01-28 11:35:14 +0800

    add adduser function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import UserList from "./components/UserList";
 
 function App() {
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,37 +1,49 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {fetchUsers} from "../store";
+import Button from "./Button";
+import {addUser} from "../store/thunks/addUser";
 
 const UserList = () => {
     const dispatch = useDispatch()
 
-    const {isLoading,data,error} = useSelector((state)=>{
-     return state.users
+    const {isLoading, data, error} = useSelector((state) => {
+        return state.users
     })
 
     useEffect(() => {
         dispatch(fetchUsers())
     }, [dispatch])
 
-    if (isLoading){
+    const handleUserAdd = () => {
+        dispatch(addUser());
+    };
+
+    if (isLoading) {
         return <div>
             Loading...
         </div>
     }
 
-    if (error){
+    if (error) {
         return <div>
             Error...
         </div>
     }
-    const renderedUsers = data.map((user)=>{
+    const renderedUsers = data.map((user) => {
         return <div key={user.id}>
             <div>{user.name}</div>
         </div>
     })
-    return <div>
-        {renderedUsers}
-    </div>
+    return (
+        <div>
+            <div className="flex flex-row justify-between m-3">
+                <h1 className="m-2 text-xl">Users</h1>
+                <Button onClick={handleUserAdd}>+ Add User</Button>
+            </div>
+            {renderedUsers}
+        </div>
+    )
 }
 
 export default UserList
